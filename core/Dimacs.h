79d82bb7f46c823e965dc30fd9424f8182f41233
Bruno Dutertre <dutebru@amazon.com>
2021-07-14 16:31:30 -0700

    Fixed incorrect warning in Dimacs parser

--- core/Dimacs.h
+++ core/Dimacs.h
@@ -33,47 +33,48 @@ namespace Glucose {
 
 template<class B, class Solver>
 static void readClause(B& in, Solver& S, vec<Lit>& lits) {
-    int     parsed_lit, var;
-    lits.clear();
-    for (;;){
-        parsed_lit = parseInt(in);
-        if (parsed_lit == 0) break;
-        var = abs(parsed_lit)-1;
-        while (var >= S.nVars()) S.newVar();
-        lits.push( (parsed_lit > 0) ? mkLit(var) : ~mkLit(var) );
-    }
+  int     parsed_lit, var;
+  lits.clear();
+  for (;;){
+    parsed_lit = parseInt(in);
+    if (parsed_lit == 0) break;
+    var = abs(parsed_lit)-1;
+    while (var >= S.nVars()) S.newVar();
+    lits.push( (parsed_lit > 0) ? mkLit(var) : ~mkLit(var) );
+  }
 }
 
 template<class B, class Solver>
 static void parse_DIMACS_main(B& in, Solver& S) {
-    vec<Lit> lits;
-    int vars    = 0;
-    int clauses = 0;
-    int cnt     = 0;
-    for (;;){
-        skipWhitespace(in);
-        if (*in == EOF) break;
-        else if (*in == 'p'){
-            if (eagerMatch(in, "p cnf")){
-                vars    = parseInt(in);
-                clauses = parseInt(in);
-                // SATRACE'06 hack
-                // if (clauses > 4000000)
-                //     S.eliminate(true);
-            }else{
-                printf("PARSE ERROR! Unexpected char: %c\n", *in), exit(3);
-            }
-        } else if (*in == 'c' || *in == 'p')
-            skipLine(in);
-        else{
-            cnt++;
-            readClause(in, S, lits);
-            S.addClause_(lits); }
+  vec<Lit> lits;
+  int vars    = 0;
+  int clauses = 0;
+  int cnt     = 0;
+  for (;;){
+    skipWhitespace(in);
+    if (*in == EOF) break;
+    else if (*in == 'p'){
+      if (eagerMatch(in, "p cnf")){
+	vars    = parseInt(in);
+	clauses = parseInt(in);
+	// SATRACE'06 hack
+	// if (clauses > 4000000)
+	//     S.eliminate(true);
+      }else{
+	printf("PARSE ERROR! Unexpected char: %c\n", *in), exit(3);
+      }
+    } else if (*in == 'c' || *in == 'p')
+      skipLine(in);
+    else{
+      cnt++;
+      readClause(in, S, lits);
+      S.addClause_(lits);
     }
-    if (vars != S.nVars())
-        fprintf(stderr, "WARNING! DIMACS header mismatch: wrong number of variables.\n");
-    if (cnt  != clauses)
-        fprintf(stderr, "WARNING! DIMACS header mismatch: wrong number of clauses.\n");
+  }
+  if (vars < S.nVars())
+    fprintf(stderr, "WARNING! DIMACS header mismatch: wrong number of variables.\n");
+  if (cnt  != clauses)
+    fprintf(stderr, "WARNING! DIMACS header mismatch: wrong number of clauses.\n");
 }
 
 // Inserts problem into solver.
