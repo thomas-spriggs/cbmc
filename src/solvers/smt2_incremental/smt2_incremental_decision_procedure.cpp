7557f489a82878949192cc6ad31cb19dc28cd4ec
Thomas Spriggs <thomas.spriggs@diffblue.com>
2021-08-27 17:50:46 +0100

    WIP

--- src/solvers/smt2_incremental/smt2_incremental_decision_procedure.cpp
+++ src/solvers/smt2_incremental/smt2_incremental_decision_procedure.cpp
@@ -1,11 +1,37 @@
 // Author: Diffblue Ltd.
 
 #include "smt2_incremental_decision_procedure.h"
+#include "convert_expr_to_smt.h"
 
-#include <solvers/smt2_incremental/smt_to_smt2_string.h>
 #include <solvers/smt2_incremental/smt_commands.h>
+#include <solvers/smt2_incremental/smt_to_smt2_string.h>
+#include <solvers/smt2_incremental/smt_terms.h>
+#include <solvers/smt2_incremental/smt_core_theory.h>
 #include <util/expr.h>
+#include <util/expr_cast.h>
+#include <util/std_expr.h>
 #include <util/string_utils.h>
+
+/// Find all sub expressions of the given \p expr which need to be expressed
+/// as separate smt commands.
+static std::vector<exprt> gather_dependent_expressions(const exprt &expr)
+{
+  std::vector<exprt> dependent_expressions;
+  expr.visit_pre([&](const exprt &expr_node)
+  {
+     if(can_cast_expr<symbol_exprt>(expr_node))
+     {
+       dependent_expressions.push_back(expr_node);
+     }
+  });
+  return dependent_expressions;
+}
+
+    	#include <iostream>
+    	#define WATCHVAR( var ) \
+    	  std::cerr << "DBG: " << __FILE__ << "(" << __LINE__ << ") " << #var << \
+    	    " = [" << (var) << "]" << std::endl
+
 
 smt2_incremental_decision_proceduret::smt2_incremental_decision_proceduret(
   std::string solver_command)
@@ -49,9 +75,18 @@ smt2_incremental_decision_proceduret::get_number_of_solver_calls() const
 
 void smt2_incremental_decision_proceduret::set_to(const exprt &expr, bool value)
 {
-  UNIMPLEMENTED_FEATURE(
-    "`set_to` (" + std::string{value ? "true" : "false"} + "):\n  " +
-    expr.pretty(2, 0));
+  PRECONDITION(can_cast_type<bool_typet>(expr.type()));
+  std::cerr << "`set_to` (" + std::string{value ? "true" : "false"} + "):\n  " +
+    expr.pretty(2, 0) << std::endl;
+
+  auto converted_term = convert_expr_to_smt(expr);
+  if(!value)
+    converted_term = smt_core_theoryt::make_not(converted_term);
+  send_to_solver(smt_assert_commandt{converted_term});
+
+  //UNIMPLEMENTED_FEATURE(
+  //  "`set_to` (" + std::string{value ? "true" : "false"} + "):\n  " +
+  //  expr.pretty(2, 0));
 }
 
 void smt2_incremental_decision_proceduret::push(
@@ -79,4 +114,22 @@ decision_proceduret::resultt smt2_incremental_decision_proceduret::dec_solve()
 {
   ++number_of_solver_calls;
   UNIMPLEMENTED_FEATURE("solving.");
+}
+
+const smt_identifier_termt &
+smt2_incremental_decision_proceduret::define_function(const exprt &expr)
+{
+  const auto existing_definition = defined_expressions.find(expr);
+  if(existing_definition != defined_expressions.cend())
+  {
+    return existing_definition->second;
+  }
+
+  smt_define_function_commandt function{
+    "B" + std::to_string(sequence()),
+    {},
+    convert_expr_to_smt(expr)};
+  defined_expressions.emplace(expr, function.identifier());
+  send_to_solver(function);
+  return function.identifier();
 }
--- src/solvers/smt2_incremental/smt2_incremental_decision_procedure.h
+++ src/solvers/smt2_incremental/smt2_incremental_decision_procedure.h
@@ -6,8 +6,15 @@
 #ifndef CPROVER_SOLVERS_SMT2_INCREMENTAL_SMT2_INCREMENTAL_DECISION_PROCEDURE_H
 #define CPROVER_SOLVERS_SMT2_INCREMENTAL_SMT2_INCREMENTAL_DECISION_PROCEDURE_H
 
+#include <solvers/smt2_incremental/smt_terms.h>
+#include <util/expr.h>
 #include <solvers/stack_decision_procedure.h>
 #include <util/piped_process.h>
+
+    	#include <iostream>
+    	#define WATCHVAR( var ) \
+    	  std::cerr << "DBG: " << __FILE__ << "(" << __LINE__ << ") " << #var << \
+    	    " = [" << (var) << "]" << std::endl
 
 class smt2_incremental_decision_proceduret final
   : public stack_decision_proceduret
@@ -37,7 +44,9 @@ protected:
   template <typename smt_typet>
   void send_to_solver(const smt_typet &smt)
   {
-    solver_process.send(smt_to_smt2_string(smt));
+    const auto solver_text = smt_to_smt2_string(smt);
+    WATCHVAR(solver_text);
+    solver_process.send(solver_text);
   }
 
   /// This is where we store the solver command for reporting the solver used.
@@ -45,6 +54,23 @@ protected:
   size_t number_of_solver_calls;
 
   piped_processt solver_process;
+
+  class sequencet {
+    size_t last_id = 0;
+  public:
+    size_t operator()()
+    {
+      return ++last_id;
+    }
+  } sequence;
+
+  std::unordered_map<exprt, smt_identifier_termt, irep_hash>
+    defined_expressions;
+
+  /// \brief Defines a new function to represent \p expr if it doesn't exisit
+  ///   already.
+  /// \returns the identifier of either the new or existing function.
+  const smt_identifier_termt &define_function(const exprt &expr);
 };
 
 #endif // CPROVER_SOLVERS_SMT2_INCREMENTAL_SMT2_INCREMENTAL_DECISION_PROCEDURE_H
