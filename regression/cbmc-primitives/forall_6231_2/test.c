d193c06a87e077d45a1021803c6bf544c5991c89
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2021-10-18 18:24:56 +0100

    Add regression tests for quantifier checks.
    These are checked against `--pointer-check` and one of them is
    sourced from the code reported in https://github.com/diffblue/cbmc/issues/6231.

---
+++ regression/cbmc-primitives/forall_6231_2/test.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include <stdlib.h>
+
+int main() {
+  char *a = malloc(128);
+
+  assert(
+    __CPROVER_forall {
+      int i ; (0 <= i && i < 1) ==> *(a+i) == *(a+i)
+    }
+  );
+
+  assert(
+  __CPROVER_forall {
+    int j; !(0 <= j && j < 1) || (j == 0 && *(a+j) == *(a+j))
+  });
+}
