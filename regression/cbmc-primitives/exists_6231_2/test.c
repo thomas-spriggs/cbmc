808509d83bdb5abf39a96127bddfcababb05f3c0
Fotis Koutoulakis <fotis.koutoulakis@diffblue.com>
2021-10-21 11:18:52 +0100

    [WIP] Add rudimentary tests for the exists quantifier.

---
+++ regression/cbmc-primitives/exists_6231_2/test.c
@@ -0,0 +1,18 @@
+#include <assert.h>
+#include <stdlib.h>
+
+// clang-format off
+int main() {
+  char *a = malloc(1);
+
+  // lots of errors with `--pointer-check` enabled
+  assert(*a == *a);
+
+  // no errors even with `--pointer-check` enabled
+  assert(
+    __CPROVER_exists {
+      int i ; (0 <= i && i < 10) && *(a+i) == *(a+i)
+    }
+  );
+}
+// clang-format on
