b362a1b95b8a6b14fe1908f586cdc8c1c5c524df
Thomas Spriggs <thomas.spriggs@diffblue.com>
2022-01-18 16:55:58 +0000

    Enable additional unit test

--- unit/util/piped_process.cpp
+++ unit/util/piped_process.cpp
@@ -107,8 +107,6 @@ TEST_CASE(
   REQUIRE(calc < 2);
 }
 
-#ifndef _WIN32
-// No Windows tests for z3 due to path and dependency issues.
 TEST_CASE(
   "Creating a sub process of z3 and read a response from an echo command.",
   "[core][util][piped_process][.z3]")
@@ -130,6 +128,7 @@ TEST_CASE(
     process.send("(exit)\n") == piped_processt::send_responset::SUCCEEDED);
 }
 
+#ifndef _WIN32
 TEST_CASE(
   "Creating a sub process and interacting with it.",
   "[core][util][piped_process][.z3]")
