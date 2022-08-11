87ad4d57c7b0b1eac2e31add9ee5200497428abf
Thomas Spriggs <thomas.spriggs@diffblue.com>
2022-08-11 17:26:57 +0100

    Make test harder!

--- regression/cbmc-incr-smt2/arrays/array_write.c
+++ regression/cbmc-incr-smt2/arrays/array_write.c
@@ -2,8 +2,8 @@ int main()
 {
   int example_array[10000];
   unsigned int index;
-  __CPROVER_assume(index < 10000);
-  example_array[index] = 42;
+  __CPROVER_assume(index < 1000);
+  example_array[index + 20] = 42;
   __CPROVER_assert(example_array[index] == 42, "Array condition");
   __CPROVER_assert(example_array[index] != 42, "Array condition");
 }
