afde9da8fdf49151bfaf99904bd9c5b946f68ea4
Thomas Spriggs <thomas.spriggs@diffblue.com>
2023-05-17 18:03:15 +0100

    Add struct encoding class

---
+++ src/solvers/smt2_incremental/struct_encoding.h
@@ -0,0 +1,27 @@
+// Author: Diffblue Ltd.
+
+#ifndef CBMC_STRUCT_ENCODINGT_H
+#define CBMC_STRUCT_ENCODINGT_H
+
+#include <util/std_expr.h>
+
+#include <memory>
+
+class namespacet;
+class boolbv_widtht;
+
+/// Encodes struct types/values into non-struct expressions/types.
+class struct_encodingt final
+{
+public:
+  explicit struct_encodingt(const namespacet &ns);
+  struct_encodingt(const struct_encodingt &other);
+  ~struct_encodingt();
+
+  typet encode(typet type) const;
+
+private:
+  std::unique_ptr<boolbv_widtht> boolbv_width;
+};
+
+#endif // CBMC_STRUCT_ENCODINGT_H
